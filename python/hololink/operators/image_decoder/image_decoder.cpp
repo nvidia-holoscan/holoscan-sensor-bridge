1d6720703cb0d5b227e9d7e1bfd1fc0eab768e6a
Ashraf Kattoura <ashraf.kattoura@realsenseai.com>
2026-01-11 19:27:05 +0200

    omit align_rgb_to_depth arg

--- python/hololink/operators/image_decoder/image_decoder.cpp
+++ python/hololink/operators/image_decoder/image_decoder.cpp
@@ -57,11 +57,10 @@ public:
     PyImageDecoder(holoscan::Fragment* fragment,
         const std::shared_ptr<holoscan::Allocator>& allocator, int cuda_device_ordinal,
         const std::string& name = "image_decoder",
-        const std::string& out_tensor_name = "",
-        const bool align_depth_to_rgb = false)
+        const std::string& out_tensor_name = "")
         : ImageDecoder(holoscan::ArgList { holoscan::Arg { "allocator", allocator },
             holoscan::Arg { "cuda_device_ordinal", cuda_device_ordinal },
-            holoscan::Arg { "out_tensor_name", out_tensor_name }, holoscan::Arg { "align_depth_to_rgb", align_depth_to_rgb } })
+            holoscan::Arg { "out_tensor_name", out_tensor_name } })
     {
         name_ = name;
         fragment_ = fragment;
@@ -81,10 +80,9 @@ PYBIND11_MODULE(_image_decoder, m)
     auto op = py::class_<ImageDecoder, PyImageDecoder, holoscan::Operator,
         std::shared_ptr<ImageDecoder>>(m, "ImageDecoderOp")
                   .def(py::init<holoscan::Fragment*, const std::shared_ptr<holoscan::Allocator>&,
-                           int, const std::string&, const std::string&, const bool>(),
+                           int, const std::string&, const std::string&>(),
                       "fragment"_a, "allocator"_a, "cuda_device_ordinal"_a = 0,
-                      "name"_a = "image_decoder"s, "out_tensor_name"_a = ""s,
-                      "align_depth_to_rgb"_a = false)
+                      "name"_a = "image_decoder"s, "out_tensor_name"_a = ""s)
                   .def("setup", &ImageDecoder::setup, "spec"_a)
                   .def("configure", &ImageDecoder::configure, "width"_a, "height"_a,
                       "pixel_format"_a, "frame_start_size"_a, "frame_end_size"_a,
